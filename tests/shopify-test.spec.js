dd5f2cc54304a996a04b5c9c07f93f688a8a12e0
sonyguntuka <sonyguntuka31@gmail.com>
2025-01-22 22:25:39 -0400

    added taking screenshots on failure
    added taking screenshots on failure

--- tests/shopify-test.spec.js
+++ tests/shopify-test.spec.js
@@ -4,22 +4,25 @@ const { test, expect } = require('@playwright/test');
 test('Shopify website test', async ({ page }) => {
     await page.goto('https://brazilianmarket.ca/');
 
-    // Example: Click on a product
+    //Click on a product
     page.waitForSelector('text=Biscoito Polvilho Argolas');
     await page.click('text=Biscoito Polvilho Argolas');
 
     await page.goto('https://brazilianmarket.ca/collections/collection-1/products/biscoito-polvilho-parmesao-150g-nazinha');
 
-    // Example: Add to cart
+    //Add to cart
     await page.waitForSelector("//button[@class='btn btn-lg btn-addtocart addtocart-js']");
     await page.click("//button[@class='btn btn-lg btn-addtocart addtocart-js']");
 
     
-    // Example: View cart and assert total price
+    //View cart and assert total price
     await page.waitForSelector("//a[@class='btn btn-border ttmodalbtn ttmodalbtn']");
     await page.click("//a[@class='btn btn-border ttmodalbtn ttmodalbtn']");
     await page.waitForSelector("//th[normalize-space()='TOTAL GERAL']");
     const totalPrice = page.locator("//th[normalize-space()='TOTAL GERAL']/following-sibling::td");
 
     expect(await totalPrice.textContent()).toBe('$3.80');
+
+    // Take a screenshot on failure
+    await page.screenshot({ path: 'screenshot.png', fullPage: true });
 });
